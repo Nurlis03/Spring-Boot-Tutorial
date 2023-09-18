c1f9c1f35560762e3c74e5e5f97a8350e1c0efbf
Nurlis <kimbiletovnurlis03@gmail.com>
2023-09-18 17:10:55 +0600

    Added JPA Repository for Student class

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,21 +4,22 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 // is a Spring annotation used to mark a class as a service component. It tells the Spring
 // container to create an instance of this class and manage its lifecycle
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
     public List<Student> getStudents() {
-		return List.of(
-			new Student(
-				1L,
-				"Mariam",
-				"mariam.jamal@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
+		return studentRepository.findAll();
 	}
 }
