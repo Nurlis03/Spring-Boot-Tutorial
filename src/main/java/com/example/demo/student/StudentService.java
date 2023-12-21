e94626ddf35e8ffd00f5df62318a93709b16331b
Nurlis <kimbiletovnurlis03@gmail.com>
2023-12-21 23:04:21 +0600

    added PUT mapping

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,12 +1,15 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 // is a Spring annotation used to mark a class as a service component. It tells the Spring
 // container to create an instance of this class and manage its lifecycle
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class StudentService {
@@ -41,4 +44,30 @@ public class StudentService {
 		}
 		studentRepository.deleteById(studentId);
 	}
+
+	@Transactional
+    public void updateStudent(Long studentId, 
+							  String name,
+							  String email) {
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new IllegalStateException(
+					"student with id " + studentId + " doew not exists"
+				));
+		if (name != null && 
+			name.length() > 0 &&
+			!Objects.equals(student.getName(), name)) {
+				student.setName(name);
+			}
+		
+			if (email != null &&
+				email.length() > 0 &&
+				!Objects.equals(student.getEmail(), email)) {
+					Optional<Student> studentOptional = studentRepository
+						.findStudentByEmail(email);
+					if (studentOptional.isPresent()) {
+						throw new IllegalStateException("email taken");
+					}
+					student.setEmail(email);
+				}
+    }
 }
